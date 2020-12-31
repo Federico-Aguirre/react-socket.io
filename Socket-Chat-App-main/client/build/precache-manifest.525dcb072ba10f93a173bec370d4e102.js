b981e6c20e66d5e3b0792671dc5b6e14b1b90fd0
Federico-Aguirre <56941776+Federico-Aguirre@users.noreply.github.com>
2020-12-30 23:36:04 -0300

    initial commit

---
+++ Socket-Chat-App-main/client/build/precache-manifest.525dcb072ba10f93a173bec370d4e102.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8864549a2a74075ada2e9ca19a1e5156",
+    "url": "/Federico-Aguirre/react-socket.io/index.html"
+  },
+  {
+    "revision": "da55638bdc097969c039",
+    "url": "/Federico-Aguirre/react-socket.io/static/css/main.fff19945.chunk.css"
+  },
+  {
+    "revision": "3eca05fb7af19f914ad1",
+    "url": "/Federico-Aguirre/react-socket.io/static/js/2.b5b365f7.chunk.js"
+  },
+  {
+    "revision": "4150a627f5138970747231fcef63f425",
+    "url": "/Federico-Aguirre/react-socket.io/static/js/2.b5b365f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da55638bdc097969c039",
+    "url": "/Federico-Aguirre/react-socket.io/static/js/main.8308b4fa.chunk.js"
+  },
+  {
+    "revision": "0261b25926fb2271ef19",
+    "url": "/Federico-Aguirre/react-socket.io/static/js/runtime-main.b9cec57b.js"
+  }
+]);
